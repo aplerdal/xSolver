d40c8d29ace40f0faab18dda20e724b55732465e
Andrew Lerdal <aplerdal01@gmail.com>
2024-09-02 14:44:53 -0500

    Why are game trees with repetition so complex

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 const boardSize int = 4
 const winCount int = 3
-const searchDepth int = 4
+const searchDepth int = 25
 
 type boardState [boardSize][boardSize]int8
 
@@ -23,6 +23,75 @@ type boardNode struct {
 	player    int8
 	parent    *boardNode
 	children  []*boardNode
+}
+type boardInfo struct {
+	board  boardState
+	winner int8
+	player int8
+	//lowest_depth uint16
+	children []boardState
+}
+
+// only works with 4x4 boards
+func hashBoard(board boardState) uint32 {
+	var buffer uint32
+	buffer = 0
+	for y := range 4 {
+		for x := range 4 {
+			buffer |= (uint32(board[x][y]+1) & 0b11) << ((y*4 + x) * 2)
+		}
+	}
+	return buffer
+}
+func printBoard(board boardState) {
+	for y := range boardSize {
+		for x := range boardSize {
+			fmt.Print(numToPiece(board[x][y]))
+		}
+		fmt.Print("\n")
+	}
+}
+func numToPiece(num int8) string {
+	if num < 0 {
+		return "\u001B[44m\u001B[37;1m  \u001B[0m"
+	} else if num > 0 {
+		return "\u001B[41m\u001B[37;1m  \u001B[0m"
+	} else {
+		return "  "
+	}
+}
+func expandBoard(b boardInfo) {
+	for y := range boardSize {
+		for x := range boardSize {
+			if b.board[x][y] == b.player {
+				addMove(&b, x, y, 1, 0)
+				addMove(&b, x, y, -1, 0)
+				addMove(&b, x, y, 0, 1)
+				addMove(&b, x, y, 0, -1)
+			}
+		}
+	}
+}
+func addMove(b *boardInfo, x int, y int, xoffset int, yoffset int) {
+	if pointInBoard(x+xoffset, y+yoffset) && b.board[x+xoffset][y+yoffset] == 0 {
+		board := b.board
+		board[x][y] = 0
+		board[x+xoffset][y+yoffset] = b.player
+		winner := checkWin(board, vec2{x: x + xoffset, y: y + yoffset})
+		child := boardInfo{
+			board:  board,
+			winner: winner,
+			player: -b.player,
+		}
+		boardMap[hashBoard(board)] = child
+		b.children = append(b.children, board)
+	}
+}
+func pointInBoard(x int, y int) bool {
+	if x >= 0 && y >= 0 && x < boardSize && y < boardSize {
+		return true
+	}
+	return false
 }
 
 // Checks for a winner given the move and the board state. Returns the winning player
@@ -89,77 +158,9 @@ func checkWin(board boardState, position vec2) int8 {
 
 	return 0
 }
-func printBoard(board boardState) {
-	for y := range boardSize {
-		for x := range boardSize {
-			fmt.Print(numToPiece(board[x][y]))
-		}
-		fmt.Print("\n")
-	}
-}
 
-// only works with 4x4 boards
-func hashBoard(board boardState) uint32 {
-	var buffer uint32
-	buffer = 0
-	for y := range 4 {
-		for x := range 4 {
-			buffer |= (uint32(board[x][y]+1) & 0b11) << ((y*4 + x) * 2)
-		}
-	}
-	return buffer
-}
-func numToPiece(num int8) string {
-	if num < 0 {
-		return "\u001B[44m\u001B[37;1m  \u001B[0m"
-	} else if num > 0 {
-		return "\u001B[41m\u001B[37;1m  \u001B[0m"
-	} else {
-		return "  "
-	}
-}
-func expandBoard(node *boardNode) {
-	for y := range boardSize {
-		for x := range boardSize {
-			if node.board[x][y] == node.player {
-				addMove(node, x, y, 1, 0)
-				addMove(node, x, y, -1, 0)
-				addMove(node, x, y, 0, 1)
-				addMove(node, x, y, 0, -1)
-			}
-		}
-	}
-	node.expanded = true
-}
-func addMove(node *boardNode, x int, y int, xoffset int, yoffset int) {
-	if pointInBoard(x+xoffset, y+yoffset) && node.board[x+xoffset][y+yoffset] == 0 {
-		board := (*node).board
-		board[x][y] = 0
-		board[x+xoffset][y+yoffset] = node.player
-		winner := checkWin(board, vec2{x: x + xoffset, y: y + yoffset})
-		child := boardNode{
-			parent:   node,
-			board:    board,
-			leaf:     winner == node.player,
-			expanded: false,
-			player:   -node.player,
-		}
-		valueMap[hashBoard(board)] = int(winner)
-		if _, ok := visitedNodes[hashBoard(board)]; ok {
-			child.leaf = true
-		}
-		node.children = append(node.children, &child)
-	}
-}
-func pointInBoard(x int, y int) bool {
-	if x >= 0 && y >= 0 && x < boardSize && y < boardSize {
-		return true
-	}
-	return false
-}
-
-var root boardNode = boardNode{
-	parent: nil,
+var boardMap map[uint32]boardInfo
+var root boardInfo = boardInfo{
 	board: boardState{
 		{1, 0, 0, -1},
 		{-1, 0, 0, 1},
@@ -167,91 +168,18 @@ var root boardNode = boardNode{
 		{-1, 0, 0, 1},
 	},
 	player: 1,
-	leaf:   false,
+	winner: 0,
 }
-
-var visitedNodes map[uint32]*boardNode
-var valueMap map[uint32]int
 
 func generateBoardTree() {
-	var node *boardNode
-
-	node = &root
-
-	viewedBoards := 0
+	boardMap = make(map[uint32]boardInfo)
+	boardMap[hashBoard(root.board)] = root
 	depth := 0
-	for !root.traversed {
-		viewedBoards++
-		fmt.Println(viewedBoards)
-		if depth > searchDepth {
-			node.traversed = true
-			if node.parent != nil {
-				node = node.parent
-				depth--
-			}
-			continue
-		}
-		if !node.expanded {
-			expandBoard(node)
-			node.expanded = true
-		}
-		allTraversed := true
-		if node.children == nil {
-			node.leaf = true
-			node.traversed = true
-			if node.parent != nil {
-				node = node.parent
-				depth--
-			}
-		} else {
-			for i, c := range node.children {
-				if !(c.leaf || c.traversed) {
-					allTraversed = false
-					node = node.children[i]
-					depth++
-					break
-				}
-			}
-			if allTraversed {
-				node.traversed = true
-				if node.parent != nil {
-					node = node.parent
-					depth--
-				}
-			}
-		}
-	}
-}
+	for {
 
-func backpropagateNode(node *boardNode) {
-	parent := node.parent
-	value := -valueMap[hashBoard(node.board)]
-	for parent != nil {
-		valueMap[hashBoard(parent.board)] += value
-		value = -value
-		parent = parent.parent
 	}
-}
-func countParents(node *boardNode) int {
-	parent := node.parent
-	count := 0
-	for parent != nil {
-		count++
-		parent = parent.parent
-	}
-	return count
 }
 
 func main() {
-	fmt.Println("Hello world. Toe solver v0")
-	visitedNodes = make(map[uint32]*boardNode)
-	valueMap = make(map[uint32]int)
-	generateBoardTree()
-	fmt.Println("Board Generated. Backpropagating...")
-	for _, node := range visitedNodes {
-		if node.leaf {
-			backpropagateNode(node)
-		}
-	}
-	printBoard(root.board)
+
 }
